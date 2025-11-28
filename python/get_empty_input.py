72713603a13e0feeec6c1fcc06224f7b30e5f24b
msifland <msifland@gmail.com>
2025-11-28 15:08:22 -0600

    auto commit

--- python/get_empty_input.py
+++
@@ -1,9 +0,0 @@
-# empty input catch
-def get_empty_input(prompt):
-	while True:
-		user_input = input(prompt)
-		#check if empty
-		if user_input.strip():
-			return user_input
-		else:
-			print("You entered nothing, please try again.")
